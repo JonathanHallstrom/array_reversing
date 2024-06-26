b1ab97ee9d15a72df01cc08743d109cda7eeee48
Jonathan Hallstrom <lmj.hallstrom@gmail.com>
2024-06-26 15:22:22 +0200

    simplify code and update gitignore for 0.13.0

--- reverser/src/root.zig
+++ reverser/src/root.zig
@@ -1,59 +1,37 @@
 const std = @import("std");
 
-fn reverseVector(comptime N: usize, comptime T: type, a: @Vector(N, T)) @Vector(N, T) {
-    const reverse_mask = comptime blk: {
-        var res: @Vector(N, i32) = undefined;
-        for (0..N) |i| {
-            res[N - i - 1] = i;
-        }
-        break :blk res;
-    };
-    return @shuffle(T, a, undefined, reverse_mask);
-}
-
-inline fn unlikely(x: bool) bool {
-    @setCold(true);
-    return x;
-}
-
-fn oneIteration(comptime T: type, a: []T, i: usize, comptime Len: comptime_int) void {
-    const n = a.len;
-    if (Len == 1) {
-        const lhs = a[i];
-        const rhs = a[n - i - 1];
-
-        a[i] = rhs;
-        a[n - i - 1] = lhs;
-        return;
-    }
-    const left_slice = a[i .. i + Len];
-    const right_slice = a[n - i - Len .. n - i];
-
-    const left: [Len]T = left_slice[0..Len].*;
-    const right: [Len]T = right_slice[0..Len].*;
-
-    const left_shuffled: [Len]T = reverseVector(Len, T, left);
-    const right_shuffled: [Len]T = reverseVector(Len, T, right);
-    @memcpy(right_slice, left_shuffled[0..]);
-    @memcpy(left_slice, right_shuffled[0..]);
-}
-
-fn assume(x: bool) void {
-    if (!x) unreachable;
-}
-
-pub fn reverseSimd(comptime T: type, a: []T) void {
+pub fn reverse(comptime T: type, items: []T) void {
     if (@sizeOf(T) == 0) return;
-    const n = a.len;
     var i: usize = 0;
-    const L1 = @max(1, 32 / @sizeOf(T));
-    while (i + L1 - 1 < n / 2) : (i += L1) {
-        oneIteration(T, a, i, L1);
-    }
-    const rem = n / 2 - i;
-    assume(rem < L1);
-    for (0..rem) |offs| {
-        oneIteration(T, a, i + offs, 1);
+    const end = items.len / 2;
+
+    if (std.simd.suggestVectorLength(T)) |simd_size| {
+        comptime var reverse_mask: @Vector(simd_size, i32) = undefined;
+        inline for (0..simd_size) |j| {
+            reverse_mask[simd_size - j - 1] = j;
+        }
+
+        if (simd_size <= end) {
+            const simd_end = end - (simd_size - 1);
+            while (i < simd_end) : (i += simd_size) {
+                const left_slice = items[i .. i + simd_size];
+                const right_slice = items[items.len - i - simd_size .. items.len - i];
+
+                var left_shuffled: [simd_size]T = @shuffle(T, left_slice[0..simd_size].*, undefined, reverse_mask);
+                var right_shuffled: [simd_size]T = @shuffle(T, right_slice[0..simd_size].*, undefined, reverse_mask);
+
+                @memcpy(right_slice, &left_shuffled);
+                @memcpy(left_slice, &right_shuffled);
+            }
+        }
+        while (i < end) : (i += 1) {
+            std.mem.swap(T, &items[i], &items[items.len - i - 1]);
+            std.mem.doNotOptimizeAway(i); // prevent unrolling of this loop, since the number of iterations is small (less than `simd_size`)
+        }
+    } else {
+        while (i < end) : (i += 1) {
+            std.mem.swap(T, &items[i], &items[items.len - i - 1]);
+        }
     }
 }
 
@@ -90,39 +68,38 @@ export fn reverse_i64(a: [*]i64, n: usize) void {
 }
 
 export fn reverse_simd_u8(a: [*]u8, n: usize) void {
-    reverseSimd(u8, a[0..n]);
+    reverse(u8, a[0..n]);
 }
 
 export fn reverse_simd_i8(a: [*]i8, n: usize) void {
-    reverseSimd(i8, a[0..n]);
+    reverse(i8, a[0..n]);
 }
 
 export fn reverse_simd_u16(a: [*]u16, n: usize) void {
-    reverseSimd(u16, a[0..n]);
+    reverse(u16, a[0..n]);
 }
 
 export fn reverse_simd_i16(a: [*]i16, n: usize) void {
-    reverseSimd(i16, a[0..n]);
+    reverse(i16, a[0..n]);
 }
 
 export fn reverse_simd_u32(a: [*]u32, n: usize) void {
-    reverseSimd(u32, a[0..n]);
+    reverse(u32, a[0..n]);
 }
 
 export fn reverse_simd_i32(a: [*]i32, n: usize) void {
-    reverseSimd(i32, a[0..n]);
+    reverse(i32, a[0..n]);
 }
 
 export fn reverse_simd_u64(a: [*]u64, n: usize) void {
-    reverseSimd(u64, a[0..n]);
+    reverse(u64, a[0..n]);
 }
 
 export fn reverse_simd_i64(a: [*]i64, n: usize) void {
-    reverseSimd(i64, a[0..n]);
+    reverse(i64, a[0..n]);
 }
 
 test {
-    @setEvalBranchQuota(1 << 20);
     var prng = std.rand.Xoroshiro128.init(0);
     const rand = prng.random();
     inline for ([_]type{ u0, u1, u2, u3, u4, u5, u6, u7, u8, u16, u24, u32, u48, u64, u128, u256 }) |T| {
@@ -139,9 +116,9 @@ test {
             defer std.testing.allocator.free(reversed);
             std.mem.reverse(T, arr);
 
-            reverseSimd(T, arr);
+            reverse(T, arr);
             try std.testing.expectEqualSlices(T, reversed, arr);
-            reverseSimd(T, arr);
+            reverse(T, arr);
             try std.testing.expectEqualSlices(T, original, arr);
         }
     }
